5942d7c81ed370d3316ce0e79f63bcd6d736dbc7
jday151r <j.day151r@gmail.com>
2019-06-23 23:05:09 -0700

    added XInput support

--- Assets/Scripts/CarController.cs
+++ Assets/Scripts/CarController.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using static MathFunctions;
+using XInputDotNetPure;
 public class CarController : MonoBehaviour
 {
     [Header("References")]
@@ -32,34 +33,63 @@ public class CarController : MonoBehaviour
     public AudioSource mainEngineSound;
     public Vector2 pitchRange; //X = minumum RPM sound, Y = maximum RPM sound
 
+    private bool[] buttonFramePresses;
+
     void Start()
     {
         if (!rBody) rBody = GetComponent<Rigidbody>();
+        buttonFramePresses = new bool[] { false, false };
+        //0 = B
+        //1 = X
     }
 
     void Update()
     {
         #region Input
 
-        if (Input.GetKey(KeyCode.W))
-            currentRPM += horsepower;
-        else if (Input.GetKey(KeyCode.S))
-            currentRPM -= Mathf.Min(brakingForce, currentRPM); //TODO this will break if minimum RPM is not 0!
-        else if (currentRPM > RPMRange.x) currentRPM -= Mathf.Min(engineSlowdown, currentRPM);
+        GamePadState controllerState = GamePad.GetState(PlayerIndex.One);
+
+        //if (Input.GetKey(KeyCode.W))
+        //    currentRPM += horsepower;
+        //else if (Input.GetKey(KeyCode.S))
+        //    currentRPM -= Mathf.Min(brakingForce, currentRPM); //TODO this will break if minimum RPM is not 0!
+        //else if (currentRPM > RPMRange.x) currentRPM -= Mathf.Min(engineSlowdown, currentRPM);
+
         //if (Input.GetKey(KeyCode.A))
         //    rotationForce -= turnSpeed;
         //if (Input.GetKey(KeyCode.D))
         //    rotationForce += turnSpeed;
-        rotationForce += Input.GetAxis("Horizontal") * turnSpeed;
-        Debug.Log(Input.GetAxis("Horizontal"));
 
-        if (Input.GetKeyDown(KeyCode.E) && currentGear < topSpeedByGear.Length)
+        //if (Input.GetKeyDown(KeyCode.E) && currentGear < topSpeedByGear.Length)
+        //{
+        //    currentGear++;
+        //    currentRPM = GetRPMFromTransmissionSpeed(topSpeedByGear[currentGear - 1], RPMRange.y, velocity.z);
+        //}
+        //if (Input.GetKeyDown(KeyCode.Q) && currentGear > 1)
+        //{
+        //    currentGear--;
+        //    currentRPM = GetRPMFromTransmissionSpeed(topSpeedByGear[currentGear - 1], RPMRange.y, velocity.z);
+        //}
+        if (controllerState.Buttons.B == ButtonState.Released) buttonFramePresses[0] = false;
+        if (controllerState.Buttons.X == ButtonState.Released) buttonFramePresses[1] = false;
+
+        if (controllerState.Triggers.Right > 0)
+            currentRPM += horsepower * controllerState.Triggers.Right * engineLoad.Evaluate(velocity.z);
+        else if (controllerState.Triggers.Left > 0)
+            currentRPM -= Mathf.Min(brakingForce * controllerState.Triggers.Left, currentRPM); //TODO this will break if minimum RPM is not 0!
+        else if (currentRPM > RPMRange.x) currentRPM -= Mathf.Min(engineSlowdown, currentRPM);
+
+        rotationForce += controllerState.ThumbSticks.Left.X * turnSpeed;
+
+        if (!buttonFramePresses[0] && controllerState.Buttons.B == ButtonState.Pressed && currentGear < topSpeedByGear.Length)
         {
+            buttonFramePresses[0] = true;
             currentGear++;
             currentRPM = GetRPMFromTransmissionSpeed(topSpeedByGear[currentGear - 1], RPMRange.y, velocity.z);
         }
-        if (Input.GetKeyDown(KeyCode.Q) && currentGear > 1)
+        if (!buttonFramePresses[1] && controllerState.Buttons.X == ButtonState.Pressed && currentGear > 1)
         {
+            buttonFramePresses[1] = true;
             currentGear--;
             currentRPM = GetRPMFromTransmissionSpeed(topSpeedByGear[currentGear - 1], RPMRange.y, velocity.z);
         }
